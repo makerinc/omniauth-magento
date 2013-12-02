41fc6f9193515d145cee28546cca80dcd6a5262d
Michael Imstepf <michael.imstepf@gmail.com>
2013-12-02 13:29:26 +1100

    Update magento.rb

--- lib/omniauth/strategies/magento.rb
+++ lib/omniauth/strategies/magento.rb
@@ -5,9 +5,9 @@ module OmniAuth
   module Strategies
     class Magento < OmniAuth::Strategies::OAuth
       option :client_options, {
-        :access_token_path  => '/oauth/token',
-        :authorize_path     => '/admin/oauth_authorize',
-        :request_token_path => '/oauth/initiate',
+        :access_token_path  => 'localhost:8888/magento/oauth/token',
+        :authorize_path     => 'localhost:8888/magento//admin/oauth_authorize',
+        :request_token_path => 'localhost:8888/magento//oauth/initiate',
         :signature_method   => 'PLAINTEXT'
       }
     end
