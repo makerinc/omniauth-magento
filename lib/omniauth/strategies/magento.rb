069a814151816a7f2c289603665c0fde01b8f75a
Michael Imstepf <michael.imstepf@gmail.com>
2013-12-03 11:27:07 +1100

    Update magento.rb

--- lib/omniauth/strategies/magento.rb
+++ lib/omniauth/strategies/magento.rb
@@ -4,13 +4,14 @@ require 'oauth/signature/plaintext'
 module OmniAuth
   module Strategies
     class Magento < OmniAuth::Strategies::OAuth
-      args [:consumer_key, :consumer_secret, :site_id]
+      #args [:consumer_key, :consumer_secret, :site_id]
       
       option :client_options, {
         :access_token_path  => '/oauth/token',
         :authorize_path     => '/oauth/authorize',
         :request_token_path => '/oauth/initiate',
-        :signature_method   => 'PLAINTEXT'
+        :signature_method   => 'PLAINTEXT',
+        :side_id => "http://localhost:8888/magento"
       }
     end
   end
