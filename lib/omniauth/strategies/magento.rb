a82f08cc69e864f032ad9e923626951ea4694573
Michael Imstepf <michael.imstepf@gmail.com>
2013-12-04 18:11:24 +1100

    Update magento.rb

--- lib/omniauth/strategies/magento.rb
+++ lib/omniauth/strategies/magento.rb
@@ -7,11 +7,10 @@ module OmniAuth
       option :name, "magento"
       
       option :client_options, {
-        :scheme             => :query_string,
-        :access_token_path  => 'oauth/token',
-        :authorize_path     => 'oauth/authorize',
-        :request_token_path => 'oauth/initiate',
-        :site               => 'http://localhost/magento'
+        :access_token_path  => "oauth/token",
+        :authorize_path     => "oauth/authorize",
+        :request_token_path => "oauth/initiate",
+        :site               => ENV["MAGENTO_URL"]
       }
     end
   end
