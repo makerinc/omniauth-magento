03d0488e8d195ce3f545b69330468f50cda8dbb6
Michael Imstepf <michael.imstepf@gmail.com>
2013-12-04 12:09:37 +1100

    Update magento.rb

--- lib/omniauth/strategies/magento.rb
+++ lib/omniauth/strategies/magento.rb
@@ -10,7 +10,7 @@ module OmniAuth
         :access_token_path  => 'oauth/token',
         :authorize_path     => 'oauth/authorize',
         :request_token_path => 'oauth/initiate',
-        :scheme => :query_string,
+        :scheme => "query_string",
         :oauth_callback => "oob"
       }
     end
