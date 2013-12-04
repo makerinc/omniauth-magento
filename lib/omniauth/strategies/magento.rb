8e820b86dd3c73588d5c02b3499ada6e90f5fa20
Michael Imstepf <michael.imstepf@gmail.com>
2013-12-04 17:29:44 +1100

    Update magento.rb

--- lib/omniauth/strategies/magento.rb
+++ lib/omniauth/strategies/magento.rb
@@ -5,13 +5,13 @@ module OmniAuth
   module Strategies
     class Magento < OmniAuth::Strategies::OAuth
 
-      args [:consumer_key, :consumer_secret, :site]
+      #args [:consumer_key, :consumer_secret, :site]
       
       option :client_options, {
         :access_token_path  => 'oauth/token',
         :authorize_path     => 'oauth/authorize',
         :request_token_path => 'oauth/initiate',
-        :scheme => :query_string,
+        :scheme => :query_string
       }
     end
   end
