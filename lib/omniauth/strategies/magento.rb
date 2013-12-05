10070ee9aa3d21715705193943e2a64a9e5cddd6
Michael Imstepf <michael.imstepf@gmail.com>
2013-12-05 12:30:12 +1100

    Update magento.rb

--- lib/omniauth/strategies/magento.rb
+++ lib/omniauth/strategies/magento.rb
@@ -4,7 +4,6 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     class Magento < OmniAuth::Strategies::OAuth
-      # Give your strategy a name.
       option :name, "magento"
       
       option :client_options, {
@@ -15,21 +14,20 @@ module OmniAuth
       }
       
       # set uid
-      uid { raw_info.id }
+      uid { @raw_info.keys.first.to_i }
 
       # set additional info
       info do
         {
-          'first_name' => raw_info.firstname,
-          'last_name' => raw_info.lastname,
-          'email' => raw_info.email
+          'first_name' => raw_info.values.first["firstname"],
+          'last_name' => raw_info.values.first["lastname"],
+          'email' => raw_info.values.first["email"]
         }
       end
 
       # get info about current user
       def raw_info
         @raw_info ||= MultiJson.decode(access_token.get('/api/rest/customers').body)
-        binding.pry
       end    
     end
   end
